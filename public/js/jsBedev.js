27613e1d7b21d00dd45cdd98d8623a22eb9fa225
martz3 <menezesmarty@gmail.com>
2023-11-27 11:05:46 -0300

    responsividade no landing + menu suspenso criado

--- public/js/jsBedev.js
+++ public/js/jsBedev.js
@@ -1,8 +1,15 @@
 const passwordInput = document.querySelector("#senhaUser")
 const eye = document.querySelector("#eye")
+const menuContent = document.getElementById("menu_content");
+
+let isLogged = false;
 
 eye.addEventListener("click", function(){
     this.classList.toggle("fa-eye-slash")
     const type = passwordInput.getAttribute("type") === "password" ? "text" : "password"
     passwordInput.setAttribute("type", type)
   })
+
+function Menu() {
+  menuContent.style.display = (menuContent.style.display === "block") ? "none" : "block";
+}
